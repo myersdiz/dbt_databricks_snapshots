a1ca5a4b72b4b7852803600ed867f7b281fac01d
myersdiz <myersdiz@gmail.com>
2023-12-16 20:00:24 -0500

    Misc updates
    Macro incremental_where renamed generate_incremental_where
    Added macro generate_n_a_row
    Updated dim_employee and dim_supplier to use new macros

--- macros/generate_incremental_where.sql
+++ macros/generate_incremental_where.sql
@@ -1,4 +1,4 @@
-{%- macro incremental_where() -%}
+{%- macro generate_incremental_where() -%}
   {%- if is_incremental() -%}
  WHERE COALESCE(dbt_valid_to, dbt_valid_from) > (SELECT MAX(COALESCE(NULLIF(dbt_valid_to,'9999-12-31 23:59:59.999'::TIMESTAMP), dbt_valid_from)) FROM {{ this }})
   {%- endif -%}
--- models/marts/sales/dimensions/dim_employee.sql
+++ models/marts/sales/dimensions/dim_employee.sql
@@ -3,4 +3,6 @@
 select {{ dbt_utils.star(from=ref('snp_employee'), except=["cdc_hash_key","dbt_scd_id","dbt_updated_at","dbt_valid_from","dbt_valid_to","dbt_current_flag"]) }}
        {{ generate_scd_columns() }}
   from {{ ref('snp_employee') }}
-{{ incremental_where() }}
+{{ generate_incremental_where() }}
+
+{{ generate_n_a_row(model_name='snp_employee', exclude_col_list=["cdc_hash_key","dbt_scd_id","dbt_updated_at","dbt_valid_from","dbt_valid_to","dbt_current_flag"]) }}
