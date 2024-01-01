daa361ea78c642dbae888a86e4a6b249681f5f82
myersdiz <myersdiz@gmail.com>
2023-12-31 22:21:08 -0500

    Misc updates
    Added sqlfluff
    Added sqlfmt
    Added trunk
    Formatted SQL files
    Added dbt_constraints support

--- models/marts/sales/dimensions/dim_employee.sql
+++ models/marts/sales/dimensions/dim_employee.sql
@@ -1,8 +1,34 @@
 {{ config(unique_key="employee_key") }}
 
-select {{ dbt_utils.star(from=ref('snp_employee'), except=["cdc_hash_key","dbt_scd_id","dbt_updated_at","dbt_valid_from","dbt_valid_to","dbt_current_flag"]) }}
-       {{ generate_scd_columns() }}
-  from {{ ref('snp_employee') }}
-{{ generate_incremental_where() }}
+select
+    {{
+        dbt_utils.star(
+            from=ref("snp_employee"),
+            except=[
+                "cdc_hash_key",
+                "dbt_scd_id",
+                "dbt_updated_at",
+                "dbt_valid_from",
+                "dbt_valid_to",
+                "dbt_current_flag",
+            ],
+        )
+    }}
+    {{ generate_scd_columns() }}
+from
+    {{ ref("snp_employee") }}
+    {{ generate_incremental_where() }}
 
-{{ generate_n_a_row(model_name='snp_employee', exclude_col_list=["cdc_hash_key","dbt_scd_id","dbt_updated_at","dbt_valid_from","dbt_valid_to","dbt_current_flag"]) }}
+    {{
+        generate_n_a_row(
+            model_name="snp_employee",
+            exclude_col_list=[
+                "cdc_hash_key",
+                "dbt_scd_id",
+                "dbt_updated_at",
+                "dbt_valid_from",
+                "dbt_valid_to",
+                "dbt_current_flag",
+            ],
+        )
+    }}
--- models/marts/sales/dimensions/dim_supplier.sql
+++ models/marts/sales/dimensions/dim_supplier.sql
@@ -1,8 +1,34 @@
 {{ config(unique_key="supplier_key") }}
 
-select {{ dbt_utils.star(from=ref('snp_supplier'), except=["cdc_hash_key","dbt_scd_id","dbt_updated_at","dbt_valid_from","dbt_valid_to","dbt_current_flag"]) }}
-       {{ generate_scd_columns() }}
-  from {{ ref('snp_supplier') }}
-{{ generate_incremental_where() }}
+select
+    {{
+        dbt_utils.star(
+            from=ref("snp_supplier"),
+            except=[
+                "cdc_hash_key",
+                "dbt_scd_id",
+                "dbt_updated_at",
+                "dbt_valid_from",
+                "dbt_valid_to",
+                "dbt_current_flag",
+            ],
+        )
+    }}
+    {{ generate_scd_columns() }}
+from
+    {{ ref("snp_supplier") }}
+    {{ generate_incremental_where() }}
 
-{{ generate_n_a_row(model_name='snp_supplier', exclude_col_list=["cdc_hash_key","dbt_scd_id","dbt_updated_at","dbt_valid_from","dbt_valid_to","dbt_current_flag"]) }}
+    {{
+        generate_n_a_row(
+            model_name="snp_supplier",
+            exclude_col_list=[
+                "cdc_hash_key",
+                "dbt_scd_id",
+                "dbt_updated_at",
+                "dbt_valid_from",
+                "dbt_valid_to",
+                "dbt_current_flag",
+            ],
+        )
+    }}
