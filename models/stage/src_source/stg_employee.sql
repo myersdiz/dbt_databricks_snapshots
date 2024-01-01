86bcefa5168c9ffe4a6b43088de383ce07df84f6
myersdiz <myersdiz@gmail.com>
2023-12-31 22:21:08 -0500

    Misc updates
    Added sqlfluff
    Added sqlfmt
    Added trunk
    Formatted SQL files
    Added dbt_constraints support

--- models/stage/src_source/stg_employee.sql
+++ models/stage/src_source/stg_employee.sql
@@ -2,5 +2,5 @@ select
     employee_id,
     upper(employee_first_name) as employee_first_name,
     upper(employee_last_name) as employee_last_name,
-    hash(employee_id, employee_first_name, employee_last_name) AS cdc_hash_key
+    hash(employee_id, employee_first_name, employee_last_name) as cdc_hash_key
 from {{ source("src_source", "src_employee") }}
